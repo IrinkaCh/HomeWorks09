d8cdc9be8d36c9cf458331bae3ccd8e62ea81a7f
IrinkaCh <123irina00@mail.ru>
2022-09-03 10:54:06 +0500

    Task 64 All Natural Numbers

---
+++ Task64AllNaturalNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Recursia(n, m);
+
+void Recursia(int n, int m)
+{
+    if (m > n) return;
+    Console.Write($"{m} ");
+    Recursia(n, m + 1);
+}
